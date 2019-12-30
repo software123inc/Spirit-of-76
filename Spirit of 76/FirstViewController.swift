4208913c664c7d1118c0423b8854e1749d09e592
Tim Newton <tim.newton@software123.com>
2019-12-30 12:23:49 -0800

    Initial Commit

---
+++ Spirit of 76/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Spirit of 76
+//
+//  Created by Tim W. Newton on 12/30/19.
+//  Copyright © 2019 Tim W. Newton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Spirit of 76/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Spirit of 76
+//
+//  Created by Tim W. Newton on 12/30/19.
+//  Copyright © 2019 Tim W. Newton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
