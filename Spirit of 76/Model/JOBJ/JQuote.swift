333979d0c0200061899b1aa26091063e2ee1c6ac
Tim Newton <tim.newton@software123.com>
2019-12-31 10:06:39 -0800

    Reset JQuote.

--- Spirit of 76/Model/JOBJ/JQuote.swift
+++ Spirit of 76/Model/JOBJ/JQuote.swift
@@ -2,7 +2,7 @@
 //  JQuote.swift
 //  Spirit of 76
 //
-//  Created by Tim W. Newton on 12/30/19.
+//  Created by Tim W. Newton on 12/31/19.
 //  Copyright © 2019 Tim W. Newton. All rights reserved.
 //
 
@@ -14,7 +14,8 @@ struct JQuotes: Codable, Equatable {
 
 struct JQuote: Codable, Equatable {
     let id: UInt
-    let writingID: UInt?
     let quotation: String
+    let context: String?
     let signerID: UInt
+    let writingID: UInt?
 }
