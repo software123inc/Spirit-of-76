41535c27a5e3e0319cc6f131d59786f680449a35
Tim Newton <tim.newton@software123.com>
2020-01-01 12:07:54 -0800

    Continue JSON importing refinements.

--- Spirit of 76/Objects/JsonManager.swift
+++ Spirit of 76/Objects/JsonManager.swift
@@ -16,31 +16,48 @@ class JSONManager {
     
     static func importRootData() {
         let files = Bundle.main.paths(forResourcesOfType: "json", inDirectory: nil)
+        let userDefault = UserDefaults.standard
         for file in files.sorted() {
             let filename = file.lastPathComponenSansExtension
-            if let f = RootFiles.init(rawValue: filename) {
+            let keyPath = "JSONManager_\(filename)"
+            let needsImport = !userDefault.bool(forKey: keyPath)
+            
+            if needsImport, let f = RootFiles.init(rawValue: filename) {
                 switch f {
                 case .countries:
                     if let json = JSONManager.importSwiftyJSON(filename) {
-                        JSONManager.seedJsonResultsToCoreData(json, type: Country.self)
+                        let importSuccessful = JSONManager.seedJsonResultsToCoreData(json, type: Country.self)
+                        print("keyPath: \(keyPath) Successful: \(importSuccessful)")
+                        userDefault.setValue(importSuccessful, forKeyPath: keyPath)
                     }
                 case .events:
                     if let json = JSONManager.importSwiftyJSON(filename) {
-                        JSONManager.seedJsonResultsToCoreData(json, type: Event.self)
+                        let importSuccessful = JSONManager.seedJsonResultsToCoreData(json, type: Event.self)
+                        print("keyPath: \(keyPath) Successful: \(importSuccessful)")
+                        userDefault.setValue(importSuccessful, forKeyPath: keyPath)
                     }
                 case .states:
                     if let json = JSONManager.importSwiftyJSON(filename) {
-                        JSONManager.seedJsonResultsToCoreData(json, type: State.self)
+                        let importSuccessful = JSONManager.seedJsonResultsToCoreData(json, type: State.self)
+                        print("keyPath: \(keyPath) Successful: \(importSuccessful)")
+                        userDefault.setValue(importSuccessful, forKeyPath: keyPath)
                     }
                 case .topics:
                     if let json = JSONManager.importSwiftyJSON(filename) {
-                        JSONManager.seedJsonResultsToCoreData(json, type: Topic.self)
+                        let importSuccessful = JSONManager.seedJsonResultsToCoreData(json, type: Topic.self)
+                        print("keyPath: \(keyPath) Successful: \(importSuccessful)")
+                        userDefault.setValue(importSuccessful, forKeyPath: keyPath)
                     }
                 case .writings:
                     if let json = JSONManager.importSwiftyJSON(filename) {
-                        JSONManager.seedJsonResultsToCoreData(json, type: Writing.self)
+                        let importSuccessful = JSONManager.seedJsonResultsToCoreData(json, type: Writing.self)
+                        print("keyPath: \(keyPath) Successful: \(importSuccessful)")
+                        userDefault.setValue(importSuccessful, forKeyPath: keyPath)
                     }
                 }
+            }
+            else {
+                debugPrint("\(keyPath) previously imported.")
             }
         }
     }
@@ -63,53 +80,68 @@ class JSONManager {
         return json
     }
     
-    private static func seedJsonResultsToCoreData<T:NSManagedObject>(_ json: JSON, type:T.Type) {
+    private static func seedJsonResultsToCoreData<T:NSManagedObject>(_ json: JSON, type:T.Type) -> Bool {
+        var isSuccessful = false
         let newManObj = type.init(context: JSONManager.viewContext)
+//        print("== Begin \(type.entity().name ?? "unknown entity") ==")
         
-        let firstRec = json[0]
-        
-        for att in newManObj.entity.attributesByName {
-            let key = att.key
-            let type = att.value.attributeType
-            
-            switch type {
-            case .stringAttributeType:
-                print("key: \(key) \(firstRec[key].string != nil ? "+" : "-")")
-            case .booleanAttributeType:
-                print("key: \(key) \(firstRec[key].bool != nil ? "+" : "-")")
-            case .integer16AttributeType:
-                print("key: \(key) \(firstRec[key].int16 != nil ? "+" : "-")")
-            case .integer32AttributeType:
-                print("key: \(key) \(firstRec[key].int32 != nil ? "+" : "-")")
-            case .integer64AttributeType:
-                print("key: \(key) \(firstRec[key].int64 != nil ? "+" : "-")")
-            case .decimalAttributeType:
-                print("key: \(key) \(firstRec[key].number?.decimalValue != nil ? "+" : "-")")
-            case .doubleAttributeType:
-                print("key: \(key) \(firstRec[key].double != nil ? "+" : "-")")
-            case .floatAttributeType:
-                print("key: \(key) \(firstRec[key].float != nil ? "+" : "-")")
-            case .dateAttributeType:
-                if let dateString = firstRec[key].string {
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "yyyy-MM-dd"
-                    let dateFromString: Date? = dateFormatter.date(from: dateString)
-                    dateFormatter.dateFormat = "MMM dd, yyyy"
-                    print("key: \(key) \(firstRec[key].string != nil ? "*" : "-")  \(dateFormatter.string(from: dateFromString ?? Date()))")
+        for (_, record):(String, JSON) in json {
+            for att in newManObj.entity.attributesByName {
+                let key = att.key
+                let type = att.value.attributeType
+                
+                switch type {
+                case .stringAttributeType:
+                    //                print("key: \(key) \(record[key].string != nil ? "+" : "-")")
+                    newManObj.setValue(record[key].string, forKey: key)
+                case .booleanAttributeType:
+                    newManObj.setValue(record[key].bool, forKey: key)
+                //                print("key: \(key) \(record[key].bool != nil ? "+" : "-")")
+                case .integer16AttributeType:
+                    newManObj.setValue(record[key].int16, forKey: key)
+//                    print("key: \(key) \(record[key].int16 != nil ? "+" : "-")")
+                case .integer32AttributeType:
+                    newManObj.setValue(record[key].int32, forKey: key)
+                //                print("key: \(key) \(record[key].int32 != nil ? "+" : "-")")
+                case .integer64AttributeType:
+                    newManObj.setValue(record[key].int64, forKey: key)
+                //                print("key: \(key) \(record[key].int64 != nil ? "+" : "-")")
+                case .decimalAttributeType:
+                    newManObj.setValue(record[key].number?.decimalValue, forKey: key)
+                //                print("key: \(key) \(record[key].number?.decimalValue != nil ? "+" : "-")")
+                case .doubleAttributeType:
+                    newManObj.setValue(record[key].double, forKey: key)
+                //                print("key: \(key) \(record[key].double != nil ? "+" : "-")")
+                case .floatAttributeType:
+                    newManObj.setValue(record[key].float, forKey: key)
+                //                print("key: \(key) \(record[key].float != nil ? "+" : "-")")
+                case .dateAttributeType:
+                    if let dateString = record[key].string {
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "yyyy-MM-dd"
+                        let dateFromString: Date? = dateFormatter.date(from: dateString)
+                        
+                        newManObj.setValue(dateFromString, forKey: key)
+                        //                    dateFormatter.dateFormat = "MMM dd, yyyy"
+                        //                    print("key: \(key) \(record[key].string != nil ? "*" : "-")  \(dateFormatter.string(from: dateFromString ?? Date()))")
+                    }
+                default:
+                    debugPrint("unhandled attribute type: \(type) !")
                 }
-                else {
-                    print("key: \(key) \(firstRec[key].string != nil ? "!" : "-")")
-                }
-            default:
-                debugPrint("key: \(key) !")
             }
         }
         
-        //                for (_, subJson):(String, JSON) in json {
-        //                    for key in newManObj.entity.attributesByName.keys {
-        //                        print("key: \(key) \(subJson[key].string != nil ? "+" : "-")")
-        //                    }
-        //                }
+        do {
+            try JSONManager.viewContext.save()
+            isSuccessful = true
+        }
+        catch {
+            debugPrint(error.localizedDescription)
+            isSuccessful = false
+        }
+        
+//        print("== End \(type.entity().name ?? "unknown entity") ==")
+        return isSuccessful
     }
 }
 
