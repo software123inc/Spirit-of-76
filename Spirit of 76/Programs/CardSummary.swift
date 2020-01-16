8c29ef3c2cd6d832af0515b0413924c20d7ee6b6
Tim Newton <tim.newton@eduserve.us>
2020-01-16 11:19:12 -0800

    Add programs path

---
+++ Spirit of 76/Programs/CardSummary.swift
@@ -0,0 +1,14 @@
+//
+//  CardSummary.swift
+//  Spirit of 76
+//
+//  Created by Tim Newton on 1/14/20.
+//  Copyright © 2020 Tim W. Newton. All rights reserved.
+//
+
+import Foundation
+
+protocol CardSummary {
+    var cardTitle:String { get }
+    var cardDetailText:String { get }
+}
